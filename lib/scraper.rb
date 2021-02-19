71b9b18c0ed37e30da1242d48c5e92a12958979a
Sara Nolen <nolensara1@gmail.com>
2021-02-19 17:12:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
         # course = Course.new
       #   course.title = post.css("h2").text
       #   course.schedule = post.css(".date").text
-        # course.description = post.css("p").text 
+        # course.description = post.css("p").text
        end
     end
 
