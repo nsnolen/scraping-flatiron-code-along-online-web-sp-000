c91208f85b4a36d308cd33bd8627527be05ce852
Sara Nolen <nolensara1@gmail.com>
2021-02-19 16:56:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,26 @@ require_relative './course.rb'
 
 class Scraper
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
+  #def print_courses
+    #self.make_courses
+  #  Course.all.each do |course|
+  #    if course.title && course.title != ""
+  #      puts "Title: #{course.title}"
+  #      puts "  Schedule: #{course.schedule}"
+  #      puts "  Description: #{course.description}"
+  #    end
+  #  end
+#  end
+
+    def get_page 
+    
     end
-  end
+
+    def make_courses 
+    end
+
+    def print_courses
+    end
   
 end
 
