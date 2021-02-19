b286f38827b7c221feaa8cd44a30401a3d40e8a5
Sara Nolen <nolensara1@gmail.com>
2021-02-19 16:53:15 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,12 +2,15 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
-  def initialize(title)
-    @title = title
+  def initialize
     @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def self.reset_all 
+    @@all.clear 
   end
 end
